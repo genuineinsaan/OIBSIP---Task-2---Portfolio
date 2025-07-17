ec2b6d13cc2d2aefb780274150dfc338d2605fe4
Shashank Srivastava <145271166+genuineinsaan@users.noreply.github.com>
2025-07-17 19:34:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Reveal scroll animation
+window.addEventListener("scroll", () => {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+});
